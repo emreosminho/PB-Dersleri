df60ec87023a86eea31b21a4b1299850e06c1b56
Emre Çanakçı <emreosmancanakci@gmail.com>
2024-05-20 21:54:15 +0300

    session kullanimi

---
+++ Sessions/set-sessions.php
@@ -0,0 +1,35 @@
+<?php
+
+    
+
+      $_SESSION["username"] = "ahmetkaya";
+      $_SESSION["password"] = "1234";
+
+    // session_unset();
+    // session_destroy();
+
+    // ini_set('session.gc_maclifetime',1800);
+    // session_start();
+
+    // session_regenerate_id(true);
+    // unset($_SESSION["username"]);
+
+    // if(isset($_SESSION["username"])){
+    //     echo $_SESSION["username"];
+    // }else{
+    //     echo "username yok";
+    // }
+
+    // echo "<br>";
+    // echo $_SESSION["password"];
+
+    session_start();
+
+    unset($_SESSION["username"]);
+
+    session_unset();
+
+    session_destroy();
+
+
+?>
